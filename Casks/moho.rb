8c79e8d73c1909265e869369e5ca14c8f1f6d6f0
horaguy <157380662+horaguy@users.noreply.github.com>
2026-09-25 15:59:29 +0900

    Update moho.rb

--- Casks/moho.rb
+++ Casks/moho.rb
@@ -2,8 +2,8 @@ cask "moho" do
   version "14.4"
   sha256 :no_check # The mirror URL does not contain the version.
 
-  # Mirror Link (primary link is https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08
-  # but it's not work with CLI)
+  # Primary link (https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08) doesn't work in CLI.
+  # So I use a mirror URL.
   url "https://dl.dropboxusercontent.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk"
   name "MOHO"
   desc "Vector based 2D Computer animation software"
